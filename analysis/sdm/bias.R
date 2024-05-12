22a2ad7246a76bd2feba81adb1896df662722039
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-05-12 13:13:47 -0700

    update sdm

--- analysis/sdm/bias.R
+++ analysis/sdm/bias.R
@@ -1,17 +1,14 @@
 
 # functions to return and calculate bias layers
-get_bias <- function(spp, rank, n = 10000, lyr = NULL, limit = 1000, cache = TRUE, dir = "outputs"){
+get_bias <- function(spp, rank, n = 10000, limit = 10000, lyr = NULL, cache = TRUE, dir = NULL){
+  if (is.null(dir)) dir <- here("analysis", "sdm", "outputs")
+
   bb <- rgbif::name_backbone(spp)
   
-  if(rank == "phylum") {
-    key <- bb$phylumKey
-    bias_path <- here(dir, paste0(key, "_", rank, "_", n, "_bias.csv"))
-  }
-  
-  if(rank == "class") {
-    key <- bb$classKey
-    bias_path <- here(dir, paste0(key, "_", rank, "_", n, "_bias.csv"))
-  }
+  if(rank == "phylum") key <- bb$phylumKey
+  if(rank == "class") key <- bb$classKey
+ 
+  bias_path <- here(dir, paste0(key, "_", rank, "_limit", limit, "_n", n, "_bias.csv"))
 
   if (file.exists(bias_path) & cache) {
     bias_bkg <- read.csv(bias_path)
@@ -19,6 +16,35 @@ get_bias <- function(spp, rank, n = 10000, lyr = NULL, limit = 1000, cache = TRU
     if (is.null(lyr)) "lyr must be provided"
     bias_bkg <- make_biasbkg(key, rank, lyr, limit, n)
   }
+  
+  return(bias_bkg)
+}
+
+make_biasbkg <- function(key, rank, lyr, limit = 10000, n = 10000, dir = NULL){
+  if (is.null(dir)) dir <- here("analysis", "sdm", "outputs")
+
+  # get key for class or phylum
+  if (is.character(key) & rank == "phylum") key <- rgbif::name_backbone(key)$phylumKey
+  if (is.character(key) & rank == "class") key <- rgbif::name_backbone(key)$classKey
+  
+  # get GBIF points
+  bias_dat <- get_biasgbif(key, rank, limit)
+
+  # make into raster
+  lyr <- get_lyr()
+  bias_lyr <- make_biaslyr(bias_dat, lyr, limit = limit)
+
+  # save output
+  terra::writeRaster(bias_lyr, here(dir, paste0(key, "_", rank, "_limit", limit, "_n", n, "_bias.tif")), overwrite = TRUE)
+  
+  # get coords for background points
+  bias_bkg <- sample_bias(bias_lyr, n)
+  write.csv(bias_bkg, here(dir, paste0(key, "_", rank, "_limit", limit, "_n", n, "_bias.csv")), row.names = FALSE)
+
+  # plot to check distribution of background points:
+  #ggplot(bias_bkg) + geom_point(aes(x = x, y = y), alpha = 0.1) + theme_void() + scale_fill_viridis_c()
+  
+  message(paste(rank, key, "complete"))
   
   return(bias_bkg)
 }
@@ -36,32 +62,19 @@ get_lyr <- function(){
   return(lyr)
 }
 
-make_biasbkg <- function(key, rank, lyr, limit = 1000, n = 10000){
-  # get key for class or phylum
-  if (is.character(key) & rank == "phylum") key <- rgbif::name_backbone(key)$phylumKey
-  if (is.character(key) & rank == "class") key <- rgbif::name_backbone(key)$classKey
-  
-  # get GBIF points
-  bias_dat <- get_biasgbif(key, rank, limit)
 
-  # make into raster
-  bias_lyr <- make_biaslyr(bias_dat, lyr)
+get_biasgbif <- function(key, rank, limit = 1000, cache = TRUE, dir = NULL){
+  if (cache){
+    if (is.null(dir)) dir <- here("analysis", "sdm", "outputs")
+    
+    bias_path <- here(dir, paste0(key, "_", rank, "_limit", limit, "_bias_gbif.csv"))
+    
+    if (file.exists(bias_path)) {
+      message("Using cached bias data")
+      return(read.csv(bias_path))
+    }
+  }
 
-  # save output
-  terra::writeRaster(bias_lyr, here("outputs", paste0(key, "_", rank, "_", n, "_bias.tif")), overwrite = TRUE)
-  
-  # get coords for background points
-  bias_bkg <- sample_bias(bias_lyr, n)
-  write.csv(bias_bkg, here("outputs", paste0(key, "_", rank, "_", n, "_bias.csv")), row.names = FALSE)
-  
-  message(paste(rank, key, "complete"))
-  
-  return(bias_bkg)
-}
-
-
-get_biasgbif <- function(key, rank, limit = 1000){
-  
   if (rank == "phylum"){
     gbif <- rgbif::occ_search(phylumKey = key, hasCoordinate = T, stateProvince = "California", limit = limit)
   }
@@ -70,24 +83,39 @@ get_biasgbif <- function(key, rank, limit = 1000){
   }
   
   bias_dat <- gbif$data
+
+  if (cache) write_csv(bias_dat, bias_path)
+
   return(bias_dat)
 }
 
-make_biaslyr <- function(bias_dat, lyr, agg = NULL){
+make_biaslyr <- function(bias_dat, lyr, limit, agg = NULL){
   # Create sf coordinates
-  bias_dat <- st_as_sf(bias_dat, coords = c("decimalLongitude", "decimalLatitude"))
+  bias_sf <- st_as_sf(bias_dat, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+
+  # Project to California Albers
+  bias_sf <- st_transform(bias_sf, crs = st_crs(3310))
+
+  # Transform raster layer to California Albers
+  lyr <- terra::project(lyr, "EPSG:3310")
   
-  # aggregate raster layer
-  if (!is.null(agg)) lyr <- terra::aggregate(lyr, agg)
+  # Aggregate raster layer
+  # If agg is not set, aggregate the layer such that ncell is approximately the number of GBIF points used to create the layer
+  if (is.null(agg)) agg <- round(ncell(lyr)/n, 0)
+  agglyr <- terra::aggregate(lyr, agg)
   
   # Create raster layer of counts for each cell
-  taxon_bias = terra::rasterize(bias_dat, lyr, fun = "count", background = NA)
+  taxon_bias <- terra::rasterize(bias_sf, agglyr, fun = "count", background = NA)
   
   # Assign NA values of bias raster to the min
-  bias_mean = taxon_bias
-  bias_mean[is.na(bias_mean)] = terra::global(bias_mean, "min", na.rm = TRUE)
-  bias_lyr = mask(bias_mean, lyr)
-  
+  bias_mean <- taxon_bias
+  bias_mean[is.na(bias_mean)] <- terra::global(bias_mean, "min", na.rm = TRUE)
+
+  # Disaggregate the layer to the original resolution 
+  # Use bilinear interpolation to fill in the gaps (smoother surface)
+  bias_mean <- terra::resample(bias_mean, lyr, method = "bilinear")
+  bias_lyr <- mask(bias_mean, lyr)
+
   return(bias_lyr)
   
 }
@@ -105,6 +133,12 @@ sample_bias <- function(bias_lyr, n = 10000){
   # Extract the sampled values (or coordinates) from the raster
   # This gives you the cell values; for coordinates, use xyFromCell
   bias_bkg <- terra::extract(bias_lyr, sample_indices, xy = TRUE)[,c("x", "y")]
+
+  # Transform coordinates to lat/long
+  bias_bkg <- st_as_sf(bias_bkg, coords = c("x", "y"), crs = st_crs(bias_lyr))
+  bias_bkg <- st_transform(bias_bkg, crs = 4326)
+  bias_bkg <- data.frame(st_coordinates(bias_bkg))
+  names(bias_bkg) <- c("x", "y")
 
   return(bias_bkg)
 }
--- analysis/sdm/sdm.R
+++ analysis/sdm/sdm.R
@@ -1,4 +1,3 @@
-
 #' Create an SDM using BIOMOD
 #'
 #' @param coordinates coordinates of species occurrence
@@ -14,16 +13,18 @@
 #' @export
 #'
 #' @examples
-sdm <- function(coordinates, envlayers, biasdat = NULL, models = "MAXENT", nbkg = 10000, output = FALSE, file.name = "", tune = TRUE){
+sdm <- function(coordinates, envlayers, biasdat = NULL, models = "MAXENT", nbkg = 10000, output = FALSE, file.name = NULL, tune = TRUE, BIOMOD_name = NULL){
   
   # Create a name for BIOMOD files (using random string of numbers so that each is unique if running in parallel)
-  #BIOMOD_name <- paste0("BIOMOD", paste(sample(0:100, 5, replace = TRUE), collapse = ""))
-  BIOMOD_name <- "BIOMOD"
+  if (is.null(BIOMOD_name)) BIOMOD_name <- paste0("BIOMOD", paste(sample(0:100, 5, replace = TRUE), collapse = ""))
+
+  # Convert envlayers to spatRaster
+  if (!inherits(envlayers, "SpatRaster")) envlayers <- rast(envlayers)
 
   # Create random background points or use bias data if provided
   if(is.null(biasdat)){
     # Generate random background points
-    abs <- sampleRandom(envlayers[[1]], nbkg, xy = TRUE)
+    abs <- terra::spatSample(envlayers[[1]], nbkg, xy = TRUE, method = "random")
     bk.df <- data.frame(pa = 0, abs[, c("x","y")])
   } else {
     # Create presence/absence data frame using bias data
@@ -43,36 +44,50 @@ sdm <- function(coordinates, envlayers, biasdat = NULL, models = "MAXENT", nbkg 
   myRespXY <- pa.df[,c("x","y")] 
   
   # Create object (myBiomodData) to contain all the previous objects within it,formatted correctly
-  myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,#presence absence vector
-                                       expl.var = envlayers, #environmental layers
-                                       resp.xy = myRespXY, #coordinates                                  
-                                       resp.name = BIOMOD_name, # name for files - doesn't really matter 
-                                       PA.nb.rep = 0, #number of required Pseudo Absences selection (if needed). 0 by Default.
-                                       ) 
+  myBiomodData <- 
+    BIOMOD_FormatingData(
+      resp.var = myResp,#presence absence vector
+      expl.var = envlayers, #environmental layers
+      resp.xy = myRespXY, #coordinates                                  
+      resp.name = BIOMOD_name, # name for files - doesn't really matter 
+      PA.nb.rep = 0, #number of required Pseudo Absences selection (if needed). 0 by Default.
+      ) 
   
   if (tune) {
     # Tune parameters
-    Biomod.tuning <- BIOMOD_Tuning(myBiomodData,
-                                   models = models)
-    
-    myBiomodOptions <- Biomod.tuning$models.options
+    myBiomodOptions <- 
+      bm_ModelingOptions(
+        models = models, 
+        strategy = 'tuned', 
+        bm.format = myBiomodData, 
+        data.type = "binary"
+      )
   } else {
-    #default parameters
-    myBiomodOptions <- BIOMOD_ModelingOptions()
+    # Default parameters
+    myBiomodOptions <- 
+      bm_ModelingOptions(
+        models = models, 
+        strategy = 'bigboss', 
+        bm.format = myBiomodData, 
+        data.type = "binary"
+      )
   }
   
   # SDM model
-  myBiomodModelOut <- BIOMOD_Modeling(bm.format = myBiomodData,
-                                    modeling.id = BIOMOD_name,
-                                    models = models,
-                                    bm.options = myBiomodOptions,
-                                    CV.strategy = 'kfold',
-                                    CV.nb.rep = 1,
-                                    CV.k = 10,
-                                    CV.do.full.models = TRUE,
-                                    metric.eval = c('TSS','ROC'),
-                                    #var.import = 2, # number of permutations for variable importance
-                                    seed.val = 42)
+  myBiomodModelOut <- 
+    BIOMOD_Modeling(
+      bm.format = myBiomodData,
+      modeling.id = BIOMOD_name,
+      models = models,
+      bm.options = myBiomodOptions,
+      CV.strategy = 'kfold',
+      CV.nb.rep = 1,
+      CV.k = 10,
+      CV.do.full.models = TRUE,
+      metric.eval = c('TSS','ROC'),
+      #var.import = 2, # number of permutations for variable importance
+      seed.val = 42
+      )
   
   # Get evaluation statistics for each model
   # Contains evaluation metric for different models and dataset. 
@@ -88,20 +103,22 @@ sdm <- function(coordinates, envlayers, biasdat = NULL, models = "MAXENT", nbkg 
     dplyr::summarize_at(c("sensitivity", "specificity", "validation"), mean, na.rm = TRUE)
   print(eval.results)
   
-  # Project model using final model (Run 11)
-  myBiomodProj <- BIOMOD_Projection(bm.mod = myBiomodModelOut,
-                                    new.env = envlayers, # project
-                                    #selected.models = paste0(BIOMOD_name, "_AllData_RUN11_", models), #which models to use
-                                    proj.name = 'BIOMOD', # name of projection
-                                    binary.meth = NULL, # evaluation method by which to choose threshold (If NULL then no binary transformation computed, else the given binary techniques will be used to transform the projection into 0/1 data.)
-                                    compress = 'xz', # compression format for object storage
-                                    build.clamping.mask = FALSE, #if TRUE, a clamping mask will be saved on hard drive
-                                    output.format = '.grd')                                
-  # Get predictions from full model 
-  predictions <- get_predictions(myBiomodProj)
-  allRun <- grepl("allData_allRun", names(predictions))
-  sdm_raster_BIOMOD <- predictions[[allRun]]
-  
+  # Project model using final model (allRun)
+  # Get allRun model name
+  model_names <- myBiomodModelOut@models.computed
+  allRun <- model_names[grep("allRun", model_names)]
+  myBiomodProj <- BIOMOD_Projection(
+    bm.mod = myBiomodModelOut,
+    new.env = envlayers, # project
+    models.chosen = allRun, #which models to use
+    proj.name = 'BIOMOD', # name of projection
+    binary.meth = NULL, # evaluation method by which to choose threshold (If NULL then no binary transformation computed, else the given binary techniques will be used to transform the projection into 0/1 data.)
+    build.clamping.mask = FALSE, #if TRUE, a clamping mask will be saved on hard drive
+    )             
+                       
+  # Get predictions
+  sdm_raster_BIOMOD <- get_predictions(myBiomodProj)
+
   # Convert raster from 0 to 1000 to 0 to 1 
   # (biomod automatically multiplies everything by 1000 to make integer values)
   sdm_raster <- sdm_raster_BIOMOD/1000 
@@ -113,20 +130,27 @@ sdm <- function(coordinates, envlayers, biasdat = NULL, models = "MAXENT", nbkg 
   if (output) write.csv(spp.eval[["eval"]], here("outputs", paste0(file.name, "_eval_results.csv")))
   
   # Delete automatically generated biomod directory
-  # DON"T DO THIS IF YOU WANT TO HINDCAST
+  # DON'T DO THIS IF YOU WANT TO HINDCAST
   #unlink(BIOMOD_name, recursive = TRUE) 
   
   return(list(eval = spp.eval, myBiomodModelOut = myBiomodModelOut, raster = sdm_raster))
 }
 
 hindcast <- function(myBiomodModelOut, new_env){
-  hindcast_proj <- BIOMOD_Projection(bm.mod = myBiomodModelOut,
-                                      new.env = new_env, # project
-                                      proj.name = 'BIOMOD', # name of projection
-                                      binary.meth = NULL, # evaluation method by which to choose threshold (If NULL then no binary transformation computed, else the given binary techniques will be used to transform the projection into 0/1 data.)
-                                      compress = 'xz', # compression format for object storage
-                                      build.clamping.mask = FALSE, #if TRUE, a clamping mask will be saved on hard drive
-                                      output.format = '.grd')                                    
+  # Get allRun model name
+  model_names <- myBiomodModelOut@models.computed
+  allRun <- model_names[grep("allRun", model_names)]
+
+  hindcast_proj <- 
+    BIOMOD_Projection(
+      bm.mod = myBiomodModelOut,
+      new.env = new_env, # project
+      proj.name = 'NEW', # name of projection
+      binary.meth = NULL, # evaluation method by which to choose threshold (If NULL then no binary transformation computed, else the given binary techniques will be used to transform the projection into 0/1 data.)
+      build.clamping.mask = FALSE, #if TRUE, a clamping mask will be saved on hard drive
+      models.chosen = allRun
+    ) 
+                                       
   # Plot SDM
   hindcast_raster <- get_predictions(hindcast_proj)
   allRun <- grepl("allData_allRun", names(hindcast_raster))
@@ -135,6 +159,44 @@ hindcast <- function(myBiomodModelOut, new_env){
   hindcast_raster <- hindcast_raster[[allRun]]/1000
 
   return(hindcast_raster)
+}
+
+hindcast_range <- function(myBiomodModelOut, new_env){
+  # Get allRun model name
+  model_names <- myBiomodModelOut@models.computed
+  allRun <- model_names[grep("allRun", model_names)]
+
+  # Binary projection
+  myBiomodProj <- BIOMOD_Projection(
+      bm.mod = myBiomodModelOut,
+      proj.name = 'RANGE',
+      new.env = envlayers,
+      models.chosen = allRun,
+      metric.binary = 'TSS',
+      build.clamping.mask = FALSE
+    )   
+
+  # Project onto new conditions
+  myBiomodProjectionNew <- 
+    BIOMOD_Projection(
+      bm.mod = myBiomodModelOut,
+      proj.name = 'NEW_RANGE',
+      new.env = new_env,
+      models.chosen = allRun,
+      metric.binary = 'TSS',
+      build.clamping.mask = FALSE
+      )
+
+  # Load current and future binary projections
+  OriginalProj <- get_predictions(myBiomodProj, metric.binary = "TSS")
+  NewProj <- get_predictions(myBiomodProjectionNew, metric.binary = "TSS")
+
+  # Compute differences (note: current = past and future = present)
+  myBiomodRangeSize <- BIOMOD_RangeSize(proj.current = NewProj, 
+                                        proj.future = OriginalProj)
+
+  plot(OriginalProj)
+  plot(myBiomodRangeSize$Diff.By.Pixel)
 }
 
 pc_var_selection <- function(envlayers, coords = NULL){
