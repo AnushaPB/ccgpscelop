243b3ffd39dcd2d368620665b3e396e1d9ad8c33
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-05-16 12:50:53 -0700

    update mmrr and gdm and gendist

--- analysis/ibdibe/gendist.R
+++ analysis/ibdibe/gendist.R
@@ -1,14 +1,14 @@
 format_dist <- function(){
-  d <- read.table(here("58-Sceloporus", paste0("58-Sceloporus_plinkdist_1ibs.mdist")))
-  id <- read.table(here("58-Sceloporus", paste0("58-Sceloporus_plinkdist_1ibs.mdist.id")))[,2]
+  d <- read.table(here("data", "processed_data","58-Sceloporus_plinkdist_1ibs.mdist"))
+  id <- read.table(here("data", "processed_data","58-Sceloporus_plinkdist_1ibs.mdist.id"))[,2]
   rownames(d) <- colnames(d) <- id
-  path <- here("data", paste0("58-Sceloporus_dist.csv"))
+  path <- here("data", "processed_data", "58-Sceloporus_dist.csv")
   write.csv(d, path)
   message("wrote dist file to:", path)
 }
 
 get_gendist <- function(){
-  gendist <- read.csv(here("data", paste0("58-Sceloporus_dist.csv")), row.names = 1)
+  gendist <- read.csv(here("data", "processed_data", "58-Sceloporus_dist.csv"), row.names = 1)
   colnames(gendist) <- row.names(gendist)
   return(gendist)
 }
