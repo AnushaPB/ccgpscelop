675b248f90ed1c4d95dc903b1ddd854e5695b0d0
E. Anne Chambers <eachambers@utexas.edu>
2024-05-20 14:36:32 -0600

    update admixture script

--- analysis/admixture/admixture.sh
+++ analysis/admixture/admixture.sh
@@ -6,20 +6,22 @@ source activate ccgpscelop
 # Create folder for outputs
 mkdir -p outputs
 
-# VCF file
-VCF=../../58-Sceloporus/58-Sceloporus_maf05_minDP5_maxDP50_rmsamp60_mm80_rmsamp20_r60.vcf.gz
+# Specify prefix for file formatting
+PREFIX="../../58-Sceloporus/58-Sceloporus_maf05_minDP5_maxDP50_rmsamp60_mm80_rmsamp20_r60"
 
 # based on code from: https://github.com/ccgproject/ccgpWorkflow/blob/227e6506cf03274be7295e9de6411bb34162d97c/workflow/modules/qc/Snakefile#L126
-# make plink files
-plink --vcf $VCF --out 58-Sceloporus --allow-extra-chr --make-bed --const-fid --noweb
+# Make plink files
+plink --vcf $PREFIX.vcf.gz --out $PREFIX --allow-extra-chr --make-bed --const-fid --noweb
 
-# fix chromosome numbers
-awk '{$1=0;print $0}' 58-Sceloporus.bim > 58-Sceloporus.bim.tmp # make temp file
-mv 58-Sceloporus.bim.tmp 58-Sceloporus.bim # replace original file with temp file
-# run admixture
-for i in {2..10}
-do
- admixture --cv 58-Sceloporus.bed $i > 58-Sceloporus.log${i}.out
-done
-# move data into subfolder
-mv *58-Sceloporus* outputs
+# Fix chromosome numbers
+awk '{$1=0;print $0}' $PREFIX.bim > $PREFIX.bim.tmp # make temp file
+mv $PREFIX.bim.tmp $PREFIX.bim # replace original file with temp file
+
+# Do two separate runs of ADMIXTURE
+mkdir -p outputs/admixture_k1-10
+for K in 1 2 3 4 5 6 7 8 9 10; do admixture --cv $PREFIX.bed $K | tee $PREFIX.${K}.out; done
+mv *58-Sceloporus* outputs/admixture_k1-10
+
+mkdir -p outputs/admixture_k3-15
+for K in 3 4 5 6 7 8 9 10 11 12 13 14 15; do admixture --cv $PREFIX.bed $K | tee $PREFIX.${K}.out; done
+mv *58-Sceloporus* outputs/admixture_k3-15
