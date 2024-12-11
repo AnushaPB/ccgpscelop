3ed2235b390008091a36e9c6f2399563283c6436
Anusha Bishop <anusha.bishop@gmail.com>
2024-12-11 12:58:22 -0800

    calc gendist

--- analysis/ibdibe/gendist.R
+++ analysis/ibdibe/gendist.R
@@ -1,14 +1,36 @@
 format_dist <- function(){
-  d <- read.table(here("data", "processed_data","58-Sceloporus_plinkdist_1ibs.mdist"))
-  id <- read.table(here("data", "processed_data","58-Sceloporus_plinkdist_1ibs.mdist.id"))[,2]
+  path <- here("analysis", "ibdibe", "outputs")
+  d <- read.table(here(path,"58-Sceloporus_annotated_pruned_0.6.mdist"))
+  id <- read.table(here(path,"58-Sceloporus_annotated_pruned_0.6.mdist.id"))[,2]
   rownames(d) <- colnames(d) <- id
-  path <- here("data", "processed_data", "58-Sceloporus_dist.csv")
+  path <- here(path, "58-Sceloporus_dist.csv")
   write.csv(d, path)
   message("wrote dist file to:", path)
 }
 
 get_gendist <- function(){
-  gendist <- read.csv(here("data", "processed_data", "58-Sceloporus_dist.csv"), row.names = 1)
+  path <- here("analysis", "ibdibe", "outputs")
+  gendist <- read.csv(here(path, "58-Sceloporus_dist.csv"), row.names = 1)
   colnames(gendist) <- row.names(gendist)
   return(gendist)
+}
+
+unfold <- function(X, scale = TRUE) {
+  x <- vector()
+  for (i in 2:nrow(X)) x <- c(x, X[i, 1:i - 1])
+  if (scale == TRUE) x <- scale(x, center = TRUE, scale = TRUE)
+  return(x)
+}
+
+dist_to_df <- function(Y, X, stdz = TRUE){
+  #Unfold X and Y
+  y <- unfold(Y, scale = stdz)
+  dfX <- purrr::map_dfc(X, unfold, scale = stdz) %>% purrr::map_dfc(as.numeric)
+
+  # Make single variable dataframe
+  df <- dfX %>%
+    dplyr::mutate(Y = y) %>%
+    tidyr::gather("var", "X", -Y)
+
+  return(df)
 }
