0211534d35d8b30a2289e1d633f64b3cc4533b06
Anusha Bishop <anusha.bishop@gmail.com>
2024-12-27 19:54:26 -0800

    update admixture

--- analysis/admixture/admixture.sh
+++ analysis/admixture/admixture.sh
@@ -7,7 +7,7 @@ source activate ccgpscelop
 mkdir -p outputs
 
 # Specify prefix for file formatting
-PREFIX="58-Sceloporus_maf05_minDP5_maxDP50_rmsamp60_mm80_rmsamp40_r60"
+PREFIX="58-Sceloporus_thinned"
 
 # based on code from: https://github.com/ccgproject/ccgpWorkflow/blob/227e6506cf03274be7295e9de6411bb34162d97c/workflow/modules/qc/Snakefile#L126
 # Copy plink files over from processed_data
@@ -22,8 +22,4 @@ mv $PREFIX.bim.tmp $PREFIX.bim # replace original file with temp file
 # Do two separate runs of ADMIXTURE
 mkdir -p outputs/admixture_k1-10
 for K in 1 2 3 4 5 6 7 8 9 10; do admixture --cv $PREFIX.bed $K | tee $PREFIX.${K}.out; done
-mv *58-Sceloporus* outputs/admixture_k1-10
-
-mkdir -p outputs/admixture_k3-15
-for K in 3 4 5 6 7 8 9 10 11 12 13 14 15; do admixture --cv $PREFIX.bed $K | tee $PREFIX.${K}.out; done
-mv *58-Sceloporus* outputs/admixture_k3-15
+mv *58-Sceloporus* outputs/
