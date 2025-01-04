05552846cd1c5d791f0d8e774c79eb32420d97e9
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-04 13:15:35 -0800

    smcpp

--- analysis/smcpp/smcpp.sh
+++ analysis/smcpp/smcpp.sh
@@ -149,10 +149,27 @@ do
 done
 wait
 
+for i in {1..9}
+do
+    (eval inds_pop$i=$(cat "${BASE_PATH}/analysis/admixture/outputs/k9_pop$i.txt" | paste -sd, -)
+    pop="inds_pop${i}"
+    process_population "pop${i}" "${!pop}" 2> pop$i.stderr) &
+done
+wait
+[1] 2495414
+[2] 2495415
+[3] 2495416
+[4] 2495418
+[5] 2495420
+[6] 2495423
+[7] 2495427
+[8] 2495433
+[9] 2495438
+
 # Run all populations together
 # Combine all populations into one
-combined_pop=$(cat "${BASE_PATH}/analysis/admixture/outputs/k9_pop"{1..9}".txt" | paste -sd, -)
-process_population "pop_all" "${combined_pop}" 2> pop_all.stderr
+#combined_pop=$(cat "${BASE_PATH}/analysis/admixture/outputs/k9_pop"{1..9}".txt" | paste -sd, -)
+#process_population "pop_all" "${combined_pop}" 2> pop_all.stderr
 
 # Check the status of the background jobs
 jobs
