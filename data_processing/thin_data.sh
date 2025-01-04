22f6e7670b22c53f093fa645b562802003dfa8a9
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-04 13:16:01 -0800

    thin stuff

--- data_processing/thin_data.sh
+++ data_processing/thin_data.sh
@@ -1,3 +1,7 @@
-PLINK=../data/ccgp_data/58-Sceloporus_annotated_complete_coords
+conda activate ccgpscelop
 
-plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_thinned
+PLINK=../data/ccgp_data/58-Sceloporus_complete_coords_annotated
+
+plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_thinned10kb
+
+plink --bfile $PLINK --bp-space 1000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_thinned1kb
