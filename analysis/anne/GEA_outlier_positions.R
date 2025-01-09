9569f2c549a465f1b553454e96b86fb5c80d67c3
eachambers <eachambers@berkeley.edu>
2025-01-09 11:43:09 -0800

    update linked SNPs and RAM usage code

--- analysis/anne/GEA_outlier_positions.R
+++ analysis/anne/GEA_outlier_positions.R
@@ -1,139 +1,94 @@
 library(here)
 library(tidyverse)
+library(data.table)
 
 # example run:
-# Rscript GEA_outlier_positions.R "./outputs/58-Sceloporus_0.6.prune.out" "./outputs/58-Sceloporus_snp_correlations.ld" "../../outputs/RDA/*/" 0.01 "together" "./outputs/"
+# Rscript GEA_outlier_positions.R "./outputs/58-Sceloporus_0.6.prune.out" "./outputs/58-Sceloporus_snp_correlations.ld" "./outputs/gea_genes.bed" "./outputs" "together"
 
 #!/usr/bin/env Rscript # leave line commented
 args = commandArgs(trailingOnly=TRUE)
 prunedout_file = args[1]	# path to .prune.out file
-input_path = args[2]		  # path to .ld correlations file and concatenated GEA results
-sig = args[3]             # alpha threshold for rdadapt p-values
-output_format = args[4]		# whether to output separate bed files for pruned out SNPs vs outlier ones; options are "separate" or "together"
-output_path = args[5]		  # output path
+corr_file = args[2] 		  # path to .ld correlations file
+gea_genes_file = args[3]  # path to gea genes bed file
+output_path = args[4]		  # path to save output files
+output_format = args[5]		# whether to output separate bed files for pruned out SNPs vs outlier ones; options are "separate" or "together"
 
 
 # Read in files -----------------------------------------------------------
 
 prunedout <- read.table(paste0(prunedout_file), col.names = "SNP")
 ldsnps <- read.table(paste0(corr_file), header = TRUE)
-zscores <- read_csv(paste0(gea_path, "/58-Sceloporus_Zscores.csv"),
-                    col_names = c("rda_snps", "axis", "loading"))
-rdadapt <- read_csv(paste0(gea_path, "/58-Sceloporus_rdadapt.csv"),
-                    col_names = c("p.values", "q.values", "rda_snps")) %>%
-  # q-values are adjusted p-values
-  filter(q.values <= sig)
-# p-value adjustment
-# fdr_pvals <- p.adjust(rdadapt$p.values, method = "fdr") %>%
-#   as.data.frame() %>%
-#   rename("fdr_pvals" = ".")
-# rdadapt <- bind_cols(rdadapt, fdr_pvals) %>%
-#   filter(fdr_pvals <= sig)
+gea_outliers <- data.table::fread(paste0(gea_genes_file), col.names = c("CHROM", "POS"))
+gea_outliers <- as.data.frame(gea_outliers)
 
 
 # Process files -----------------------------------------------------------
 
+# Unite cols in ldsnps to get CHROM_POS formatting
+ldsnps <- ldsnps %>% 
+  dplyr::select(-c(SNP_A, SNP_B)) %>%
+  tidyr::unite("SNP_A", CHR_A:BP_A, sep = "_") %>%
+  tidyr::unite("SNP_B", CHR_B:BP_B, sep = "_")
+
+# Combine gea outliers CHROM and POS cols
+gea_outliers <- gea_outliers %>%
+  tidyr::unite("SNP", CHROM:POS, sep = "_")
+
+# Split up prunedout to remove REF and ALT cols
+prunedout <- prunedout %>% 
+  tidyr::extract(SNP, c("CHROM", "POS", "REF", "ALT"), regex = "(.*)_([^_]+)_([^_]+)_([^_]+)$") %>% 
+  dplyr::select(CHROM, POS) %>%
+  tidyr::unite("SNP", CHROM:POS, sep = "_")
+
 # Only retain correlations for outlier SNPs
-process_corr <- function(dat, rda_snps, prunedout) {
-  corr <- dat %>%
-    dplyr::filter(SNP_A %in% rda_snps |
-                    SNP_B %in% rda_snps) %>%
-    dplyr::mutate(SNP_A_category = case_when(SNP_A %in% rda_snps ~ "outlier",
-                                             SNP_A %in% prunedout$SNP ~ "prunedout"),
-                  SNP_B_category = case_when(SNP_B %in% rda_snps ~ "outlier",
-                                             SNP_B %in% prunedout$SNP ~ "prunedout")) %>%
-    dplyr::filter(SNP_A_category != SNP_B_category)
+corr <- ldsnps %>%
+  # Only retain comparisons involving outlier SNPs
+  dplyr::filter(SNP_A %in% gea_outliers$SNP | SNP_B %in% gea_outliers$SNP) %>%
+  dplyr::mutate(SNP_A_category = case_when(SNP_A %in% gea_outliers$SNP ~ "outlier",
+                                          SNP_A %in% prunedout$SNP ~ "prunedout"),
+                SNP_B_category = case_when(SNP_B %in% gea_outliers$SNP ~ "outlier",
+                                          SNP_B %in% prunedout$SNP ~ "prunedout")) %>%
+  # Remove comparisons between outliers or between pruned SNPs
+  dplyr::filter(SNP_A_category != SNP_B_category)
 
-  if (nrow(corr) == 0) message("No correlations found between outliers and pruned out SNPs, stopping")
+if (nrow(corr) == 0) stop("No correlations found between outliers and pruned out SNPs, stopping")
 
-  corr_a <- corr %>%
-    dplyr::filter(SNP_A_category == "prunedout") %>%
-    dplyr::rename(pruned_chrom = CHR_A,
-                  pruned_pos = BP_A,
-                  pruned_snp = SNP_A,
-                  outlier_chrom = CHR_B,
-                  outlier_pos = BP_B,
-                  outlier_snp = SNP_B) %>%
-    dplyr::select(-c(SNP_A_category, SNP_B_category))
+corr_a <- corr %>%
+  dplyr::filter(SNP_A_category == "prunedout") %>%
+  dplyr::rename(pruned_snp = SNP_A, outlier_snp = SNP_B) %>%
+  dplyr::select(-c(SNP_A_category, SNP_B_category))
 
-  corr_b <- corr %>%
-    dplyr::filter(SNP_B_category == "prunedout") %>%
-    dplyr::rename(pruned_chrom = CHR_B,
-                  pruned_pos = BP_B,
-                  pruned_snp = SNP_B,
-                  outlier_chrom = CHR_A,
-                  outlier_pos = BP_A,
-                  outlier_snp = SNP_A) %>%
-    dplyr::select(-c(SNP_A_category, SNP_B_category))
+corr_b <- corr %>%
+  dplyr::filter(SNP_B_category == "prunedout") %>%
+  dplyr::rename(pruned_snp = SNP_B, outlier_snp = SNP_A) %>%
+  dplyr::select(-c(SNP_A_category, SNP_B_category))
 
-  final <- bind_rows(corr_a, corr_b)
-
-  return(final)
-}
-
-# Z-scores
-corr_z <- process_corr(dat = ldsnps, rda_snps = zscores$rda_snps, prunedout = prunedout)
-
-# RDadapt SNPs
-corr_p <- process_corr(dat = ldsnps, rda_snps = rdadapt$rda_snps, prunedout = prunedout)
-
-if (nrow(corr_z) == 0) corr_z <- NULL
-if (nrow(corr_p) == 0) corr_p <- NULL
+final <- bind_rows(corr_a, corr_b)
 
 
 # Export files ------------------------------------------------------------
 
 # Export correlation info
-if (!is.null(corr_z)) {
-  write_csv(corr_z, file = paste0(output_path, "/58-Sceloporus_corr_Zscores.csv"))
-}
-if (!is.null(corr_p)) {
-  write_csv(corr_p, file = paste0(output_path, "/58-Sceloporus_corr_rdadapt.csv"))
-}
+write_csv(final, file = paste0(output_path, "/58-Sceloporus_corr.csv"))
 
 # Export bed files
-bed_helper <- function(corr) {
-  bed_pruned <- corr %>%
-    dplyr::select(pruned_chrom, pruned_pos, pruned_snp) %>%
-    distinct() %>%
-    rename(chrom = pruned_chrom,
-           chromStart = pruned_pos) %>%
-    mutate(chromEnd = chromStart) %>%
-    dplyr::select(-pruned_snp)
-  return(bed_pruned)
-}
+bed_pruned <- final %>%
+  dplyr::select(pruned_snp) %>%
+  distinct() %>%
+  tidyr::extract(pruned_snp, c("chrom", "chromStart"), regex = "(.*)_([^_]+)$") %>%
+  mutate(chromEnd = chromStart)
 
-if (!is.null(corr_z)) {
-  bed_z <- bed_helper(corr_z)
-}
-if (!is.null(corr_p)) {
-  bed_p <- bed_helper(corr_p)
-}
-
-bed_rdadapt <- rdadapt %>%
-  tidyr::extract(rda_snps, c("chrom", "chromStart", "ref", "alt"),
-                 regex = "(.*)_([^_]+)_([^_]+)_([^_]+)$") %>%
-  # tidyr::separate(rda_snps, sep = "_", into = c("chrom", "chromStart", "ref", "alt")) %>%
-  dplyr::mutate(chromEnd = as.numeric(chromStart),
-                chromStart = as.numeric(chromStart)) %>%
-  dplyr::select(-c(ref, alt, q.values, p.values))
-
-bed_zscores <- zscores %>%
-  tidyr::extract(rda_snps, c("chrom", "chromStart", "ref", "alt"),
-                 regex = "(.*)_([^_]+)_([^_]+)_([^_]+)$") %>%
-  # tidyr::separate(rda_snps, sep = "_", into = c("chrom", "chromStart", "ref", "alt")) %>%
-  dplyr::mutate(chromEnd = as.numeric(chromStart),
-                chromStart = as.numeric(chromStart)) %>%
-  dplyr::select(-c(ref, alt, axis, loading))
+bed_outliers <- final %>%
+  dplyr::select(outlier_snp) %>%
+  distinct() %>%
+  tidyr::extract(outlier_snp, c("chrom", "chromStart"), regex = "(.*)_([^_]+)$") %>%
+  mutate(chromEnd = chromStart)
 
 if (output_format == "separate") {
-  if (!is.null(bed_p)) write_tsv(bed_p, file = paste0(output_path, "/58-Sceloporus_gea_prunedout_rdadapt.bed"))
-  if (!is.null(bed_z)) write_tsv(bed_z, file = paste0(output_path, "/58-Sceloporus_gea_prunedout_Zscores.bed"))
-  write_tsv(bed_rdadapt, file = paste0(output_path, "/58-Sceloporus_gea_rda_rdadapt.bed"))
-  write_tsv(bed_zscores, file = paste0(output_path, "/58-Sceloporus_gea_rda_Zscores.bed"))
+  readr::write_tsv(bed_pruned, file = paste0(output_path, "/58-Sceloporus_gea_corr_prunedout.bed"))
+  readr::write_tsv(bed_outliers, file = paste0(output_path, "/58-Sceloporus_gea_corr_outliers.bed"))
 }
 
 if (output_format == "together") {
-  if (!is.null(bed_p)) write_tsv(bind_rows(bed_p, bed_rdadapt), file = paste0(output_path, "/58-Sceloporus_gea_rda_rdadapt.bed"))
-  if (!is.null(bed_z)) write_tsv(bind_rows(bed_z, bed_zscores), file = paste0(output_path, "/58-Sceloporus_gea_rda_Zscores.bed"))
+  readr::write_tsv(bind_rows(bed_pruned, bed_outliers), file = paste0(output_path, "/58-Sceloporus_gea_corr.bed"))
 }
