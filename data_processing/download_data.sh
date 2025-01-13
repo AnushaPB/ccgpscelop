d73a065c0355a662cd6ccf7c24cd5782c798e58b
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-12 20:02:29 -0800

    update sources for rsync

--- data_processing/download_data.sh
+++ data_processing/download_data.sh
@@ -11,14 +11,14 @@ mkdir -p data/ccgp_data
 # Download specific files to the data directory
 
 # Callable sites:
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/58-Sceloporus_callable_sites.bed data/ccgp_data
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/58-Sceloporus_callable_sites.bed ../data/ccgp_data
 
 # Coordinates:
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/QC/58-Sceloporus.coords.txt data/ccgp_data
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/QC/58-Sceloporus.coords.txt ../data/ccgp_data
 
 # VCF files:
-rsync --list-only hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/58-Sceloporus_complete_coords_annotated.vcf.gz
 rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/58-Sceloporus_complete_coords_annotated.vcf.gz ../data/ccgp_data
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/58-Sceloporus_complete_coords_annotated.vcf.gz.csi ../data/ccgp_data
 rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/58-Sceloporus_complete_coords_pruned_0.6.vcf.gz ../data/ccgp_data
 
 # Depth and missingness:
@@ -26,12 +26,5 @@ rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/QC/58-Sceloporus.idep
 rsync -avz hgdownload.soe.ucsc.edu::ccgp/58-Sceloporus_chr/QC/58-Sceloporus.imiss ../data/ccgp_data
 
 # RDA outputs:
-mkdir -p analysis/gea/outputs
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/ analysis/gea/outputs
-#rsync --list-only hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/
-
-rsync --list-only hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/admixture/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/admixture/Q_files/58-Sceloporus.7.Q analysis/admixture/outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/admixture/Q_files/58-Sceloporus.8.Q analysis/admixture/outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/admixture/Q_files/58-Sceloporus.9.Q analysis/admixture/outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/admixture/58-Sceloporus.fam analysis/admixture/outputs/
+mkdir -p ../analysis/gea/outputs
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/ ../analysis/gea/outputs
