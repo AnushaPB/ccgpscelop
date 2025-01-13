dfd5023f70b3935350ea3c72d8ad5bc4ea4ca52e
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-12 20:02:47 -0800

    update thinning of data

--- data_processing/thin_data.sh
+++ data_processing/thin_data.sh
@@ -1,7 +1,5 @@
 conda activate ccgpscelop
 
-PLINK=../data/ccgp_data/58-Sceloporus_complete_coords_annotated
+PLINK=../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6.vcf.gz
 
-plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_thinned10kb
-
-plink --bfile $PLINK --bp-space 1000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_thinned1kb
+plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_pruned_0.6_thinned_10kb
