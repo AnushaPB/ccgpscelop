7278fe5e0d2395f20bf4c4f868a6fcbad4d8d4d1
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-13 10:52:04 -0800

    cleanup gea code

--- analysis/gea/p1_process_rda.R
+++ analysis/gea/p1_process_rda.R
@@ -4,14 +4,11 @@ library(tidyverse)
 rda_results <- read_csv(here("analysis", "gea", "outputs", "58-Sceloporus_RDA_outliers_full_rdadapt.csv"))
 #rda_results <- read_csv(here("analysis", "gea", "outputs", "58-Sceloporus_RDA_outliers_full_Zscores.csv"))
 
-rda_results %>% filter(grepl("Scaffold_1__1_contigs__length_4124712_470887", locus))
-
 rda_adj <- 
   rda_results %>% 
   dplyr::rename(scaffold = scaff) %>%
   # Used a holm correction for multiple testing because it is more conservative
   mutate(p.adj = p.adjust(p.values, method = "holm")) %>%
-  #filter(p.values < 0.01) %>% 
   mutate(
     # Pull out the digit in ...[digit]_[bp]_[bp] pattern
     start = as.integer(str_extract(locus, "(?<=_)[0-9]+(?=_[A-Z]+_[A-Z]+)")),
