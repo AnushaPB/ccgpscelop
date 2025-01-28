05686338734a732b4e2c1cafbf2c2d77ee62c5af
Anusha Bishop <anusha.bishop@gmail.com>
2025-01-27 22:02:03 -0800

    switch to pd

--- analysis/smcpp/get_top10_scaffolds.py
+++ analysis/smcpp/get_top10_scaffolds.py
@@ -1,33 +1,25 @@
-from collections import defaultdict
 import pandas as pd
 
-# Read BED file
 bed_file = "../../data/ccgp_data/58-Sceloporus_callable_sites.bed"
-scaffold_lengths = defaultdict(int)
+bed_df = pd.read_csv(bed_file, sep="\t", header=None, names=["scaffold", "start", "end"])
 
-# Calculate scaffold lengths
-with open(bed_file, 'r') as f:
-    for line in f:
-        scaffold, start, end = line.strip().split()
-        scaffold_lengths[scaffold] += int(end) - int(start)
-
-# Create a DataFrame
-df = pd.DataFrame(list(scaffold_lengths.items()), columns=['Scaffold', 'Length'])
+# Calculate lengths for each row, sum by scaffold, and reset the index
+df = (bed_df.assign(length=bed_df["end"] - bed_df["start"])
+      .groupby("scaffold")["length"].sum()
+      .reset_index())
 
 # Sort by length
-df = df.sort_values(by='Length', ascending=False)
+df = df.sort_values(by="length", ascending=False)
 
 # Get the top 10 longest scaffolds
 top_10 = df.head(10)
-
-# Print the sorted DataFrame
 print(top_10)
 
-# Write the top 10 to a new file
-top_10[['Scaffold']].to_csv("outputs/top_10_scaffolds.txt", sep="\t", index=False, header=False)
+# Write the top 10 to a file which will be read in for SMC++
+top_10[['scaffold']].to_csv("outputs/top_10_scaffolds.txt", sep="\t", index=False, header=False)
 
 # Calculate what percentage of the genome is covered by the top 10 scaffolds
-total_length = df['Length'].sum()
-top_10_length = top_10['Length'].sum()
+total_length = df['length'].sum()
+top_10_length = top_10['length'].sum()
 percentage_covered = (top_10_length / total_length) * 100
 print(f"Percentage of the genome covered by the top 10 scaffolds: {percentage_covered:.2f}%")
