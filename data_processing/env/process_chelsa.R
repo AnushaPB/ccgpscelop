00c6017e8df8160727173fca5e4f57b9bf2f9494
Anusha Bishop <anusha.bishop@gmail.com>
2025-02-27 19:33:07 -0800

    add chelsa process

--- data_processing/env/process_chelsa.R
+++ data_processing/env/process_chelsa.R
@@ -48,7 +48,10 @@ trace <- rast(files)
 trace <- crop(trace, ca)
 trace <- mask(trace, ca)
 
+writeRaster(here("data", "env", "california_chelsa_trace21k.tif"), overwrite = TRUE)
+
 # Calculate variance
+trace <- rast(here("data", "env", "california_chelsa_trace21k.tif"))
 trace_var <- app(trace, var, na.rm = TRUE)
 trace_sd <- app(trace, sd, na.rm = TRUE)
 names(trace_var) <- c("trace21k_var")
@@ -56,16 +59,52 @@ names(trace_sd) <- c("trace21k_sd")
 trace_stack <- c(trace_var, trace_sd)
 
 png(here("data_processing", "env", "trace21k_var.png"))
-plot(trace_stack)
+plot(trace_stack, col = viridis::turbo(100))
 dev.off()
 
-df <- read_csv(here("analysis", "genetic_diversity", "outputs", "model_df.csv"))
-df$trace_sd <- terra::extract( trace_sd, df[,c("x", "y")], ID = FALSE)[,1]
-df$trace_var <- terra::extract( trace_var, df[,c("x", "y")], ID = FALSE)[,1]
+coords <- get_coords(sf = TRUE)
+mod_df <- read_csv(here("analysis", "genetic_diversity", "outputs", "model_df.csv"))
+time_key <- data.frame(timeid = rev(seq(-200, 20, 1)), timepoint = seq(0, 22, 0.1))
+time_vals <- 
+  terra::extract(trace, coords, ID = FALSE) %>%
+  mutate(SampleID = coords$SampleID) %>% 
+  pivot_longer(-SampleID, names_to = "variable", values_to = "value") %>%
+  filter(grepl("_bio01_", variable)) %>%
+  mutate(timeid = gsub("_V1.0", "", gsub("CHELSA_TraCE21k_bio01_", "", variable))) %>%
+  mutate(timeid = as.numeric(timeid)) %>%
+  # Convert to k BP
+  left_join(time_key) %>%
+  # Convert to BP
+  mutate(yearbp = timepoint*1000) %>%
+  left_join(mod_df) %>%
+  drop_na(Ho)
+
+cumsum <- 
+  time_vals %>% 
+  group_by(SampleID) %>%
+  arrange(yearbp) %>%
+  mutate(change = abs(value - lag(value, default = first(value)))) %>%
+  #mutate(change = ifelse(change > 1, change, 0)) %>%
+  summarise(cumsum = sum(change, na.rm = TRUE)) %>%
+  ungroup()
+
+df <- bind_cols(mod_df) %>% left_join(coords) %>% drop_na(Ho)
+coords <- coords %>% filter(SampleID %in% df$SampleID)
+df$trace_sd <- terra::extract(trace_sd, coords, ID = FALSE)[,1]
+df$trace_var <- terra::extract(trace_var, coords, ID = FALSE)[,1]
+df <- df %>% drop_na(trace_sd, trace_var, Ho)
 summary(lm(scale(Ho) ~ scale(trace_sd), data = df))
 summary(lm(scale(Ho) ~ scale(trace_var), data = df))
 
 pdf(here("data_processing", "env", "trace21k_var.pdf"), width = 4, height = 4)
 gglm("trace_sd", "Ho", df)
 gglm("trace_var", "Ho", df)
+gglm("cumsum", "Ho", df)
+ggpartial("trace_sd", "Ho", c("trace_sd", "Q"), df)
+ggpartial("lgm_lig", "Ho", c("lgm_lig", "Q"), df)
+gglm("lgm_lig", "Ho", df)
+ggplot(time_vals, aes(x = yearbp, y = value)) +
+  geom_line(aes(group = SampleID)) +
+  theme_bw()
 dev.off()
+
