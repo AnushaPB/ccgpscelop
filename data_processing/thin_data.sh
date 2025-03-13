5fa63e2fc5f8ec5d92055aa21ccfb1030c218a63
Anusha Bishop <anusha.bishop@gmail.com>
2025-03-12 17:28:13 -0700

    change data source

--- data_processing/thin_data.sh
+++ data_processing/thin_data.sh
@@ -1,6 +1,6 @@
 conda activate ccgpscelop
 
-PLINK=../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6
+PLINK=../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6_chr
 
 # Make plink fil
-plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_pruned_0.6_thinned_10kb
+plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_pruned_0.6_thinned_10kb_chr
--- data_processing/vcf_to_plink.sh
+++ data_processing/vcf_to_plink.sh
@@ -1,24 +1,23 @@
 conda activate ccgpscelop
 
 VCF=../data/ccgp_data/58-Sceloporus_complete_coords_annotated.vcf.gz
-PRUNED=../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6.vcf.gz
+PRUNED=../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6.vcf.gz
 
 # Convert VCF to PLINK
 plink --vcf $VCF --make-bed --out ../data/ccgp_data/58-Sceloporus_complete_coords_annotated --allow-extra-chr --const-fid > vcf_to_plink.out 2> vcf_to_plink.err
 
-plink --vcf $PRUNED --make-bed --out ../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6 --allow-extra-chr --const-fid > vcf_to_plink.out 2> vcf_to_plink.err
+plink --vcf $PRUNED --make-bed --out ../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6 --allow-extra-chr --const-fid > vcf_to_plink.out 2> vcf_to_plink.err
 
 #plink --vcf 58-Sceloporus_complete_coords_annotated.vcf.gz --make-bed --out 58-Sceloporus_complete_coords_annotated --allow-extra-chr --const-fid
 
-#plink --vcf 58-Sceloporus_complete_coords_pruned_0.6.vcf.gz --make-bed --out 58-Sceloporus_complete_coords_pruned_0.6 --allow-extra-chr --const-fid
+#plink --vcf 58-Sceloporus_annotated_pruned_0.6.vcf.gz --make-bed --out 58-Sceloporus_annotated_pruned_0.6 --allow-extra-chr --const-fid
 
 # Filter out chromosomes
 # 70215227 out of 71154824 
 # List scaffold names from PLINK file
 awk '{print $1}' ../data/ccgp_data/58-Sceloporus_complete_coords_annotated.bim | sort | uniq > scaffold_names.txt
-
 plink --bfile ../data/ccgp_data/58-Sceloporus_complete_coords_annotated --allow-extra-chr --chr 1 2 3 4 5 6 7 8 9 10 11 Scaffold_13__1_contigs__length_49873245 --make-bed --out ../data/ccgp_data/58-Sceloporus_complete_coords_annotated_chr
 
-# Before count: 50,181,443
-# After count: 4,382,248
-plink --bfile ../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6 --allow-extra-chr --chr 1 2 3 4 5 6 7 8 9 10 11 Scaffold_13__1_contigs__length_49873245 --make-bed --out ../data/ccgp_data/58-Sceloporus_complete_coords_pruned_0.6_chr
+# full count: 50,181,443
+# pruned count: 4,382,248
+plink --bfile ../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6 --allow-extra-chr --chr 1 2 3 4 5 6 7 8 9 10 11 Scaffold_13__1_contigs__length_49873245 --make-bed --out ../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6_chr
