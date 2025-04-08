0823702cfe5985cb6faf187c71fe80ee0663e1c3
Anusha Bishop <anusha.bishop@gmail.com>
2025-04-07 20:18:30 -0700

    minor

--- analysis/gea/p1_process_rda.R
+++ analysis/gea/p1_process_rda.R
@@ -6,15 +6,18 @@ rda_results <- read_csv(here("analysis", "gea", "outputs", "58-Sceloporus_RDA_ou
 
 rda_results <- 
   rda_results %>% 
-  select(-path) %>%
+  dplyr::select(-path) %>%
   dplyr::rename(scaffold = scaff, locus = rda_snps) %>%
   mutate(
     # Pull out the digit in ...[digit]_[bp]_[bp] pattern
     start = as.integer(str_extract(locus, "(?<=_)[0-9]+(?=_[A-Z]+_[A-Z]+)")),
     end = start
-  )
+  ) %>%
+  # IMPORTANT: remove duplicate SNPs (e.g., SNPs associated with more than one axis)
+  distinct(locus, axis, loading)
 
 print(paste("Number of significant loci:", nrow(rda_results)))
+#  "Number of significant loci: 1602968"
 
 # Write out csv file
 write_csv(rda_results, here("analysis", "gea", "outputs", "bio1ndvi_significant_snps.csv"))
