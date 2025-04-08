dd9499cd59b665c82127fd16950ded6b6ea59785
Anusha Bishop <anusha.bishop@gmail.com>
2025-04-07 20:17:31 -0700

    figure out go

--- analysis/gea/figure_out_go.R
+++ analysis/gea/figure_out_go.R
@@ -1,16 +1,12 @@
 
 library(gprofiler2)
-gorth(query = c("Q8BLA8"), source_organism = "mmusculus",
-target_organism = "hsapiens")
-gorth(query = c("Q8K424"), source_organism = "mmusculus",
-target_organism = "hsapiens", numeric_ns = "UNIPROT_GN")
 
+# Get two gene IDs for mouse/human
+gorth(query = c("Q8BLA8"), source_organism = "mmusculus", target_organism = "hsapiens")
+gorth(query = c("Q8K424"), source_organism = "mmusculus", target_organism = "hsapiens")
 ids <- c("Q8BLA8", "Q8K424")
 
-gorth(query = c("Q8K424"), source_organism = "mmusculus",
-target_organism = "hsapiens", numeric_ns = "UNIPROT_GN")
-
-ids <- "TRPV3"
+# When you run gorth, you will get nothing because the mouse IDs are used instead of the human ids
 go_result <-
   gost(
       query = ids, 
@@ -31,7 +27,28 @@ go_result <-
       highlight = TRUE
   )
 
-a <- go_result$result
-
-genes_org$full_name[11514]
-gorth(query = c("Q4KLT3"), source_organism = "xlaevis", target_organism = "hsapiens")
+# You can convert to humans like this
+gorth(query = c("Q8BLA8"), source_organism = "mmusculus", target_organism = "hsapiens")
+gorth(query = c("Q8K424"), source_organism = "mmusculus", target_organism = "hsapiens")
+# THe gorth says the ortholog name is this, which can be used to query gprofiler2
+ids <- "TRPV3"
+go_result <-
+  gost(
+      query = ids, 
+      organism = "hsapiens", 
+      ordered_query = FALSE, 
+      multi_query = FALSE, 
+      significant = TRUE,
+      exclude_iea = FALSE, 
+      measure_underrepresentation = FALSE, 
+      evcodes = TRUE, 
+      user_threshold = 0.05, 
+      correction_method = "g_SCS", 
+      domain_scope = "annotated", 
+      custom_bg = NULL, 
+      numeric_ns = "", 
+      sources = NULL, 
+      as_short_link = FALSE, 
+      highlight = TRUE
+  )
+go_result$result %>% head()
