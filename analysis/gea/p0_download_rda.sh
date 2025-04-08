5b4bc420732470e016bbb7f1bdd89858d2fbefdd
Anusha Bishop <anusha.bishop@gmail.com>
2025-04-07 20:18:19 -0700

    fix rda download

--- analysis/gea/p0_download_rda.sh
+++ analysis/gea/p0_download_rda.sh
@@ -1,11 +1,5 @@
 # Run in analysis/gea/
 # RDA outputs:
 mkdir -p outputs
-#rsync -avz --include '*/' --include '*rdadapt.csv*' --exclude '*' hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/subsets/RDA/ outputs/
-
-rsync --list-only hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/
-
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/58-Sceloporus_RDA_cortest_full.csv outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/58-Sceloporus_RDA_peakRAM.csv outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/58-Sceloporus_RDA_outliers_full_Zscores.csv outputs/
-rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA/58-Sceloporus_RDA_outliers_full_rdadapt.csv outputs/
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA_no_pca/58-Sceloporus_RDA_outliers_full_Zscores.csv ../analysis/gea/outputs
+rsync -avz hgdownload.soe.ucsc.edu::ccgp/algatr/58-Sceloporus/RDA_no_pca/58-Sceloporus_RDA_cortest_full.csv  ../analysis/gea/outputs
