643897794d8af9d4f55eff9cd40755f1cc05083f
Anusha Bishop <anusha.bishop@gmail.com>
2025-05-06 11:42:42 -0700

    mc

--- analysis/gea/functions_go.R
+++ analysis/gea/functions_go.R
@@ -97,71 +97,76 @@ convert_to_orgcode <- function(organism) {
 }
 
 
-run_go <- function(query, org_key){  
-    go_result <-
-        gost(
-            query = query, 
-            organism = org_key, ordered_query = FALSE, 
-            multi_query = FALSE, significant = TRUE, exclude_iea = FALSE, 
-            measure_underrepresentation = FALSE, evcodes = TRUE, 
-            user_threshold = 0.05, correction_method = "g_SCS", 
-            domain_scope = "annotated", custom_bg = NULL, 
-            numeric_ns = "", sources = NULL, as_short_link = FALSE, highlight = TRUE
-        )
-
-    return(go_result)
+run_go <- function(query, custom_bg, organism, correction_method = "fdr") {
+  gost(
+    query = query, 
+    organism = organism, 
+    ordered_query = FALSE, 
+    multi_query = FALSE, 
+    significant = FALSE, 
+    exclude_iea = FALSE, 
+    measure_underrepresentation = FALSE, 
+    evcodes = TRUE, 
+    user_threshold = 0.05, 
+    correction_method = correction_method,
+    domain_scope = "custom_annotated",
+    custom_bg = custom_bg, 
+    numeric_ns = "", 
+    sources = NULL, 
+    as_short_link = FALSE, 
+    highlight = FALSE
+  )
 }
 
+# go_correction <- function(go_result, all_genes){
+#   gea_genes <- 
+#     go_result$result %>% 
+#     as_tibble() %>%
+#     # Just keep GO terms
+#     filter(grepl("GO", source)) %>%
+#     dplyr::select(intersection_size, query_size, term_size, term_name, source, term_id, p_value)
 
-go_correction <- function(go_result, all_genes){
-  gea_genes <- 
-    go_result$result %>% 
-    as_tibble() %>%
-    # Just keep GO terms
-    filter(grepl("GO", source)) %>%
-    dplyr::select(intersection_size, query_size, term_size, term_name, source, term_id, p_value)
-
-  # Note: if you get an error here it means that there are NAs in query_size, which should not be the case - make sure no filtering was done on all_genes (all genes should not be filtered by p)
-  comparison <- 
-    right_join(all_genes, gea_genes, by = "term_id") %>%
-    rowwise() %>%
-    mutate(fish_p = go_fish(query_size, intersection_size, query_size_all, intersection_size_all))
+#   # Note: if you get an error here it means that there are NAs in query_size, which should not be the case - make sure no filtering was done on all_genes (all genes should not be filtered by p)
+#   comparison <- 
+#     right_join(all_genes, gea_genes, by = "term_id") %>%
+#     rowwise() %>%
+#     mutate(fish_p = go_fish(query_size, intersection_size, query_size_all, intersection_size_all))
     
-  # Term size all and term size should be the same
-  stopifnot(comparison$term_size_all == comparison$term_size)
+#   # Term size all and term size should be the same
+#   stopifnot(comparison$term_size_all == comparison$term_size)
 
-  # Get significant terms 
-  # *precision:* The precision of the enrichment, calculated as the ratio of intersection_size to query_size. Precision measures the proportion of relevant genes (i.e., genes associated with the GO term) in the query set.
-  # *recall:* The recall of the enrichment, calculated as the ratio of intersection_size to term_size. Recall measures the proportion of genes associated with the GO term that are present in the reference set.
-  sig <- 
-    comparison %>% 
-    # Correct p-values using fdr
-    mutate(fish_p = p.adjust(fish_p, method = "fdr")) %>%
-    filter(fish_p < 0.05) %>% 
-    arrange(fish_p) %>% 
-    mutate(precision = intersection_size / query_size,
-           recall = intersection_size / term_size) %>%
-    dplyr::select(term_name, term_id, source, fish_p, query_size_all, intersection_size, precision, recall)
+#   # Get significant terms 
+#   # *precision:* The precision of the enrichment, calculated as the ratio of intersection_size to query_size. Precision measures the proportion of relevant genes (i.e., genes associated with the GO term) in the query set.
+#   # *recall:* The recall of the enrichment, calculated as the ratio of intersection_size to term_size. Recall measures the proportion of genes associated with the GO term that are present in the reference set.
+#   sig <- 
+#     comparison %>% 
+#     # Correct p-values using fdr
+#     mutate(fish_p = p.adjust(fish_p, method = "fdr")) %>%
+#     filter(fish_p < 0.05) %>% 
+#     arrange(fish_p) %>% 
+#     mutate(precision = intersection_size / query_size,
+#            recall = intersection_size / term_size) %>%
+#     dplyr::select(term_name, term_id, source, fish_p, query_size_all, intersection_size, precision, recall)
 
-  return(sig)
-}
+#   return(sig)
+# }
 
-go_fish <- function(query_size, intersection_size, query_size_all, intersection_size_all) {
-  # Calculate the counts for the contingency table
-  a <- intersection_size  # Number of GEA genes in the category
-  b <- query_size - intersection_size  # Number of GEA genes not in the category
-  c <- intersection_size_all - intersection_size  # Number of non-GEA Sceloporus genes in the category
-  d <- query_size_all - intersection_size_all  # Number of non-GEA Sceloporus genes not in the category
+# go_fish <- function(query_size, intersection_size, query_size_all, intersection_size_all) {
+#   # Calculate the counts for the contingency table
+#   a <- intersection_size  # Number of GEA genes in the category
+#   b <- query_size - intersection_size  # Number of GEA genes not in the category
+#   c <- intersection_size_all - intersection_size  # Number of non-GEA Sceloporus genes in the category
+#   d <- query_size_all - intersection_size_all  # Number of non-GEA Sceloporus genes not in the category
   
-  # Create the contingency table
-  contingency_table <- matrix(c(a, b, c, d), nrow = 2, byrow = TRUE)
+#   # Create the contingency table
+#   contingency_table <- matrix(c(a, b, c, d), nrow = 2, byrow = TRUE)
   
-  # Perform Fisher's exact test (one-sided for enrichment)
-  result <- fisher.test(contingency_table, alternative = "greater")
+#   # Perform Fisher's exact test (one-sided for enrichment)
+#   result <- fisher.test(contingency_table, alternative = "greater")
   
-  # Return the p-value
-  return(result$p.value)
-}
+#   # Return the p-value
+#   return(result$p.value)
+# }
 
 
 
@@ -204,19 +209,40 @@ go_table <- function(df, n = 5) {
   return(gt_table)
 }
 
-go_topbp <- function(x, head = 5, arrange_by = "intersection_size", descending = TRUE){
+go_sig <- function(x, category, p = 0.05){
   x %>% 
-  filter(grepl("GO:BP", source)) %>%
-  dplyr::select(term_name, intersection_size, precision, fish_p) %>%
-  arrange(if (descending) desc(!!sym(arrange_by)) else !!sym(arrange_by)) %>%
-  head(head)
+    filter(source == category) %>%
+    dplyr::select(term_name, intersection_size, precision, recall, p_value) %>%
+    filter(p_value < p) %>%
+    arrange(p_value)
 }
 
-go_topmf <- function(x, head = 5){
-  x %>% 
-  filter(grepl("GO:MF", source)) %>%
-  dplyr::select(term_name, intersection_size, precision, fish_p) %>%
-  arrange(if (descending) desc(!!sym(arrange_by)) else !!sym(arrange_by)) %>%
-  head(head)
+go_sigbp <- function(x, p = 0.05) {
+  go_sig(x, "GO:BP", p)
 }
+
+go_sigmf <- function(x, p = 0.05) {
+  go_sig(x, "GO:MF", p)
+}
+
+go_top <- function(x, category, n = 10, arrange_by = "intersection_size", descending = TRUE, precision_cutoff = 0) {
+  message("Filtering ", category, " terms with precision >= ", precision_cutoff)
+  message("Arranging by ", arrange_by, " in ", if (descending) "descending" else "ascending", " order")
+  message("Returning top ", n, " terms")
+  x %>%
+    filter(source == category) %>%
+    filter(precision >= precision_cutoff) %>%
+    dplyr::select(term_name, intersection_size, precision, recall, p_value) %>%
+    arrange(if (descending) dplyr::desc(!!sym(arrange_by)) else !!sym(arrange_by)) %>%
+    head(n)
+}
+
+go_topbp <- function(x, n = 10, arrange_by = "precision", descending = TRUE, precision_cutoff = 0) {
+  go_top(x, "GO:BP", n, arrange_by, descending, precision_cutoff)
+}
+
+go_topmf <- function(x, n = 5, arrange_by = "precision", descending = TRUE, precision_cutoff = 0) {
+  go_top(x, "GO:MF", n, arrange_by, descending, precision_cutoff)
+}
+
 
