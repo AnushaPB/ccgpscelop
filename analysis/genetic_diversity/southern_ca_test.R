11334924a1450236908dbfcc46c360f2ce6f5fc0
Anusha Bishop <anusha.bishop@gmail.com>
2025-11-17 00:22:55 -0800

    proving to myself it's not just southern CA

---
+++ analysis/genetic_diversity/southern_ca_test.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+source(here("general_functions.R"))
+model_df <- read_csv(here("analysis", "genetic_diversity", "outputs", "model_df.csv"))
+head(model_df)
+pop_df <- get_pops()
+model_df <- left_join(model_df, pop_df)
+
+no_socal <- model_df %>% filter(!cluster %in% c(6, 8))
+lm(Ho ~ csi_past + bio1 + tmean_dif + Q + log_fri + pfs_surfac, data = no_socal) %>% summary()
+unique(no_socal$cluster)
+
+quick_plot <- function(x) {
+  plt1 <- 
+    ggplot(data = model_df, aes_string(x = x, y = "Ho")) +
+    geom_point(aes(color = factor(cluster))) +
+    geom_smooth(method = "lm", se = FALSE) +
+    theme_classic() +
+    labs(color = "Cluster") +
+    scale_color_viridis_d(option = "turbo") +
+    ggpubr::stat_cor()
+  
+  plt2 <-
+    ggplot(data = no_socal, aes_string(x = x, y = "Ho")) +
+    geom_point(aes(color = factor(cluster))) +
+    geom_smooth(method = "lm", se = FALSE) +
+    theme_classic() +
+    labs(color = "Cluster") +
+    scale_color_viridis_d(option = "turbo") +
+    ggpubr::stat_cor()
+
+  cowplot::plot_grid(plt1, plt2, labels = c("A. All populations", "B. Excluding Southern CA"), ncol = 2)
+}
+
+pdf(here("analysis", "genetic_diversity", "plots", "southern_ca_test.pdf"), width = 10, height = 4)
+quick_plot("csi_past")
+quick_plot("bio1")
+quick_plot("tmean_dif")
+quick_plot("log_fri")
+quick_plot("pfs_surfac")
+dev.off()
