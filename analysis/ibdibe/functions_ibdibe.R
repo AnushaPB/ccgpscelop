3c5d20ba071c7e9bd00e374a5a30da3a83e19e2b
Anusha Bishop <anusha.bishop@gmail.com>
2026-01-21 11:11:31 -0800

    update ibdibe

--- analysis/ibdibe/functions_ibdibe.R
+++ analysis/ibdibe/functions_ibdibe.R
@@ -49,110 +49,41 @@ dist_to_df <- function(Y, X, stdz = TRUE){
   return(df)
 }
 
-
-gdm_plot_diss <- function(gdm_model) {
-  # Extract and rename the observed, predicted, and ecological values
-  obs <- tidyr::as_tibble(gdm_model$observed) %>% dplyr::rename(observed = value)
-  pred <- tidyr::as_tibble(gdm_model$predicted) %>% dplyr::rename(predicted = value)
-  ecol <- tidyr::as_tibble(gdm_model$ecological) %>% dplyr::rename(ecological = value)
-
-  # Combine into one data frame
-  dat <- dplyr::bind_cols(obs, pred, ecol)
-  n <- nrow(dat)
-
-  # Create theoretical GDM overlay curves
-  overlayX_ecol <- seq(from = min(dat$ecological), to = max(dat$ecological), length.out = n)
-  overlayY_ecol <- 1 - exp(-overlayX_ecol)
-  overlayX_pred <- overlayY_pred <- seq(from=min(dat$predicted), to=max(dat$predicted), length.out = n)
-
-  # Plot 1: Observed vs predicted ecological distance
-  plot_ecol <-
-    ggplot2::ggplot(dat) +
-    ggplot2::geom_hex(ggplot2::aes(x = ecological, y = observed)) +
-    ggplot2::geom_line(
-      data = data.frame(x = overlayX_ecol, y = overlayY_ecol),
-      mapping = ggplot2::aes(x = x, y = y),
-      color = "red", size = 1
-    ) +
-    ggplot2::theme_classic() +
-    ggplot2::scale_y_continuous(expand = c(0, 0)) +
-    ggplot2::scale_fill_viridis_c() +
-    ggplot2::xlab("Predicted ecological distance") +
-    ggplot2::ylab("Observed compositional dissimilarity") +
-    ggplot2::labs(fill = "Sample\ncount") 
-
-  # Plot 2: Observed vs predicted compositional dissimilarity
-  plot_pred <-
-    ggplot2::ggplot(dat) +
-    ggplot2::geom_hex(ggplot2::aes(x = predicted, y = observed)) +
-    ggplot2::geom_line(
-      data = data.frame(x = overlayX_pred, y = overlayY_pred),
-      mapping = ggplot2::aes(x = x, y = y),
-      color = "red", size = 1
-    ) +
-    ggplot2::theme_classic() +
-    ggplot2::scale_y_continuous(expand = c(0, 0)) +
-    ggplot2::scale_fill_viridis_c() +
-    ggplot2::xlab("Predicted compositional dissimilarity") +
-    ggplot2::ylab("Observed compositional dissimilarity") +
-    ggplot2::labs(fill = "Sample\ncount") 
-
-  # Combine plots
-  cowplot::plot_grid(plot_ecol, plot_pred, nrow = 1)
-}
-
-
 gdm_plot_isplines <- function(gdm_model, env, coords, scales = "free", nrow = NULL, ncol = NULL) {
-  
   if (!inherits(coords, "sf")) coords <- sf::st_as_sf(coords, coords = c("x", "y"), crs = sf::st_crs(env))
+  splineDat <- gdm::isplineExtract(gdm_model)
 
-  gdm_model_splineDat <- gdm::isplineExtract(gdm_model)
-
-  gdm_spline_df <- 
-    dplyr::bind_rows(
-      data.frame(gdm_model_splineDat$x, var = "x", ID = 1:nrow(gdm_model_splineDat$x)), 
-      data.frame(gdm_model_splineDat$y, var = "y", ID = 1:nrow(gdm_model_splineDat$y))
-    ) %>%
+  gdm_spline_df <- dplyr::bind_rows(
+    data.frame(splineDat$x, var = "x", ID = seq_len(nrow(splineDat$x))),
+    data.frame(splineDat$y, var = "y", ID = seq_len(nrow(splineDat$y)))
+  ) %>%
     tidyr::pivot_longer(-c("var", "ID"), names_to = "name", values_to = "value") %>%
     tidyr::pivot_wider(names_from = "var", values_from = "value") %>%
     dplyr::mutate(name = factor(name, levels = unique(name))) %>%
     dplyr::mutate(name = gsub("_", " ", name)) %>%
-    dplyr::mutate(name = gsub("Geographic", "Geographic distance", name)) 
+    dplyr::mutate(name = gsub("Geographic", "Geographic distance", name)) %>%
+    dplyr::mutate(x = ifelse(name == "Geographic distance", x/1000, x))
 
-  # Extract unique x positions for each variable to use as rug lines
-  rug_df <- 
-    env %>% 
-    pivot_longer(everything(), names_to = "name", values_to = "x") %>%
-    bind_rows(data.frame(name = "Geographic distance", x = as.vector(st_distance(coords)))) %>%
-    mutate(name = gsub("_", " ", name)) 
-  yend <- min(gdm_spline_df$y)
-  ystart <- yend - (max(gdm_spline_df$y) * 0.05)
+  rug_df <- env %>%
+    tidyr::pivot_longer(dplyr::everything(), names_to = "name", values_to = "x") %>%
+    dplyr::mutate(name = gsub("_", " ", name)) %>%
+    dplyr::bind_rows(data.frame(name = "Geographic distance", x = as.vector(sf::st_distance(coords))/1000))
 
-  plt <-
-    ggplot2::ggplot(gdm_spline_df) +
-      # Add small vertical rug lines at bottom
-      ggplot2::geom_segment(
-        data = rug_df,
-        ggplot2::aes(x = x, xend = x, y = ystart, yend = yend),
-        alpha = 0.1,
-        color = "#7a7a7a"
-      ) +
-      ggplot2::geom_line(ggplot2::aes(x = x, y = y)) +
-      ggplot2::facet_wrap(~name, scales = scales, nrow = nrow, ncol = ncol, strip.position = "bottom") +
-      ggplot2::theme_bw() +
-      ggplot2::scale_y_continuous(expand = c(0, 0)) +
-      ggplot2::xlab("") +
-      ggplot2::ylab("Partial Regression Distance") +
-      ggplot2::theme(
-        strip.placement = "outside",
-        strip.background = ggplot2::element_blank(),
-        panel.grid = ggplot2::element_blank(),
-        strip.text = ggplot2::element_text(size = 11),
-        axis.title.y = ggplot2::element_text(size = 11)
-      )
+  yend <- min(gdm_spline_df$y, na.rm = TRUE)
+  ystart <- yend - (max(gdm_spline_df$y, na.rm = TRUE) * 0.05)
 
-  return(plt)
+  ggplot2::ggplot(gdm_spline_df) +
+    ggplot2::geom_segment(data = rug_df, ggplot2::aes(x = x, xend = x, y = ystart, yend = yend, col = name), alpha = 0.1) +
+    ggplot2::geom_line(ggplot2::aes(x = x, y = y, col = name), linewidth = 1) +
+    ggplot2::facet_wrap(~name, scales = scales, nrow = nrow, ncol = ncol, strip.position = "bottom") +
+    ggplot2::theme_bw() +
+    ggplot2::scale_y_continuous(expand = c(0, 0)) +
+    ggplot2::ylab("Partial Regression Distance") +
+    ggplot2::xlab("") +
+    ggplot2::theme(strip.placement = "outside", strip.background = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(), strip.text = ggplot2::element_text(size = 15), axis.title.y = ggplot2::element_text(size = 14), axis.text = ggplot2::element_text(size = 12), legend.position = "none") +
+    ggplot2::scale_color_manual(values = c("Geographic distance" = "black", setNames(scales::hue_pal()(length(setdiff(unique(gdm_spline_df$name), "Geographic distance")) + 1)[-1], setdiff(unique(gdm_spline_df$name), "Geographic distance"))))
 }
+
 
 # convert from matrix/data.frame/sf to formatted df
 coords_to_df <- function(coords) {
@@ -273,10 +204,12 @@ gdm_pc_helper <- function(pcaSamp, pcaRast, pcaRastRGB, coords, x = "PC1", y = "
     varpc %>% 
     mutate(varnames = 
       case_when(
-        varnames == "xCoord" ~ "Longitude",
-        varnames == "yCoord" ~ "Latitude",
+        varnames == "xCoord" ~ "X coordinate",
+        varnames == "yCoord" ~ "Y coordinate",
         TRUE ~ varnames)
-    )
+    ) %>%
+    # Replace underscores with spaces for plotting
+    mutate(varnames = gsub("_", " ", varnames))
 
   # Get PC values for each coord
   pcavals <- data.frame(terra::extract(pcaRast, coords, ID = FALSE))
@@ -293,6 +226,22 @@ gdm_pc_helper <- function(pcaSamp, pcaRast, pcaRastRGB, coords, x = "PC1", y = "
     v1 = scl * scldat * varpc[, x],
     v2 = scl * scldat * varpc[, y]
   )
+
+  # Normalize loadings to [0,1] for RGB
+  scale01 <- function(x) {
+    rng <- range(x, na.rm = TRUE)
+    if (diff(rng) == 0) return(rep(0.5, length(x)))
+    (x - rng[1]) / diff(rng)
+  }
+
+  varpc <- varpc %>%
+    mutate(
+      R = scale01(PC1),
+      G = scale01(PC2),
+      B = scale01(PC3),
+      rgb_col = rgb(R, G, B)
+    )
+
 
   # GET RGB VALS FOR EACH COORD----------------------------------------------------------------------------------------
 
@@ -338,16 +287,27 @@ gdm_pc_helper <- function(pcaSamp, pcaRast, pcaRastRGB, coords, x = "PC1", y = "
     #ggplot2::geom_point(data = rastvals, ggplot2::aes_string(x = x, y = y), col = rastpcacols, size = 4, alpha = 0.02) +
 
     # Plot coord values
-    ggplot2::geom_point(data = pcavals, ggplot2::aes_string(x = x, y = y), fill = pcacols, col = "black", pch = 21, size = 3) +
+    ggplot2::geom_point(data = pcavals, ggplot2::aes_string(x = x, y = y), fill = pcacols, col = "black", pch = 21, size = 2) +
 
     # Plot variable vectors
-    ggplot2::geom_text(
+    ggplot2::geom_segment(data = varpc, ggplot2::aes(x = 0, y = 0, xend = v1, yend = v2, col = varnames), arrow = ggplot2::arrow(length = ggplot2::unit(0.3, "cm")), linewidth = 0.8, alpha = 1) +
+    ggrepel::geom_label_repel(
       data = varpc,
-      ggplot2::aes(x = v1, y = v2, label = varnames),
+      aes(x = v1, y = v2, label = varnames, col = varnames),
       size = 4,
-      vjust = -0.4 # nudge labels upward
+
+      # white background behind text
+      fill = "white",
+      label.size = 0.25,      # border thickness (0 = no border)
+      label.r = unit(0.15, "lines"),  # rounded corners
+
+      # breathing room from points
+      box.padding = 0.8,
+      point.padding = 0.8,
+
+      # Colors
+      segment.color = NA
     ) +
-    ggplot2::geom_segment(data = varpc, ggplot2::aes(x = 0, y = 0, xend = v1, yend = v2), arrow = ggplot2::arrow(length = ggplot2::unit(0.2, "cm"))) +
 
     # Plot formatting
     ggplot2::coord_equal() +
@@ -357,8 +317,24 @@ gdm_pc_helper <- function(pcaSamp, pcaRast, pcaRastRGB, coords, x = "PC1", y = "
       panel.grid.major = ggplot2::element_blank(),
       panel.grid.minor = ggplot2::element_blank(),
       axis.line = ggplot2::element_blank(),
-      aspect.ratio = 1
+      aspect.ratio = 1, 
+      legend.position = "none"
+    ) +
+
+  # Add manual color scale so that Latitude and Longitude are always black
+  ggplot2::scale_color_manual(
+    values = c(
+      "X coordinate"  = "black",
+      "Y coordinate" = "black",
+      setNames(
+        scales::hue_pal()(
+          length(setdiff(unique(varpc$varnames),
+                        c("X coordinate", "Y coordinate"))) + 1
+        )[-1],   # ← skip first default ggplot color
+        setdiff(unique(varpc$varnames), c("X coordinate", "Y coordinate"))
+      )
     )
+  )
 
   # Build plot without PC axes displayed
   if (display_axes == FALSE) {
@@ -373,4 +349,50 @@ gdm_pc_helper <- function(pcaSamp, pcaRast, pcaRastRGB, coords, x = "PC1", y = "
 
   # Plot
   return(plot)
+}
+
+gdm_plot_diss <- function(gdm_model, type = NULL, line = TRUE) {
+  obs <- tidyr::as_tibble(gdm_model$observed) %>% dplyr::rename(observed = value)
+  pred <- tidyr::as_tibble(gdm_model$predicted) %>% dplyr::rename(predicted = value)
+  ecol <- tidyr::as_tibble(gdm_model$ecological) %>% dplyr::rename(ecological = value)
+  dat <- dplyr::bind_cols(obs, pred, ecol)
+  n <- nrow(dat)
+
+  overlayX_ecol <- seq(min(dat$ecological), max(dat$ecological), length.out = n)
+  overlayY_ecol <- 1 - exp(-overlayX_ecol)
+  overlayX_pred <- overlayY_pred <- seq(min(dat$predicted), max(dat$predicted), length.out = n)
+
+  plot_ecol <- ggplot2::ggplot(dat) +
+    ggplot2::geom_hex(ggplot2::aes(x = ecological, y = observed)) +
+    ggplot2::theme_classic() +
+    ggplot2::scale_y_continuous(expand = c(0, 0)) +
+    ggplot2::scale_fill_viridis_c() +
+    ggplot2::xlab("Predicted ecological distance") +
+    ggplot2::ylab("Observed dissimilarity") +
+    ggplot2::labs(fill = "Sample\ncount") +
+    ggplot2::theme(axis.title = ggplot2::element_text(size = 14), axis.text = ggplot2::element_text(size = 12))
+
+  if (line) plot_ecol <- plot_ecol + ggplot2::geom_line(data = data.frame(x = overlayX_ecol, y = overlayY_ecol), ggplot2::aes(x = x, y = y), color = "tomato2", size = 1)
+
+  plot_pred <- ggplot2::ggplot(dat) +
+    ggplot2::geom_hex(ggplot2::aes(x = predicted, y = observed)) +
+    ggplot2::theme_classic() +
+    ggplot2::scale_y_continuous(expand = c(0, 0)) +
+    ggplot2::scale_fill_viridis_c() +
+    ggplot2::xlab("Predicted dissimilarity") +
+    ggplot2::ylab("Observed dissimilarity") +
+    ggplot2::labs(fill = "Sample\ncount") +
+    ggplot2::theme(axis.title = ggplot2::element_text(size = 14), axis.text = ggplot2::element_text(size = 12), legend.title = ggplot2::element_text(size = 14), legend.text = ggplot2::element_text(size = 12))
+
+  if (line) plot_pred <- plot_pred + ggplot2::geom_line(data = data.frame(x = overlayX_pred, y = overlayY_pred), ggplot2::aes(x = x, y = y), color = "tomato2", size = 1)
+
+  legend <- cowplot::get_legend(plot_pred)
+  plot_pred <- plot_pred + ggplot2::theme(legend.position = "none")
+  plot_ecol <- plot_ecol + ggplot2::theme(legend.position = "none")
+
+  plt <- cowplot::plot_grid(cowplot::plot_grid(plot_ecol, plot_pred, nrow = 1), legend, nrow = 1, rel_widths = c(1, 0.15))
+
+  if (is.null(type)) return(plt)
+  if (type == "ecological") return(plot_ecol + ggplot2::theme(legend.position = "right"))
+  if (type == "predicted") return(plot_pred + ggplot2::theme(legend.position = "right"))
 }
