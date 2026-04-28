39f5cdfe1a8f350dcd83960795eb8be9c6622048
Anusha Bishop <anusha.bishop@gmail.com>
2026-04-28 10:45:50 -0700

    add individual count check

--- data_processing/p4_thin_data.sh
+++ data_processing/p4_thin_data.sh
@@ -4,3 +4,6 @@ PLINK=../data/ccgp_data/58-Sceloporus_annotated_pruned_0.6_chr
 
 # Make plink fil
 plink --bfile $PLINK --bp-space 10000 --make-bed --allow-extra-chr --out ../data/ccgp_data/58-Sceloporus_pruned_0.6_thinned_10kb_chr
+
+# Count number of individuals in thinned PLINK file
+wc -l ../data/ccgp_data/58-Sceloporus_pruned_0.6_thinned_10kb_chr.fam # 257
