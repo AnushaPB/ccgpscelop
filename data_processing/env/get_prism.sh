32bac49526cd2a3d7398c8b1ecf662a6145e9767
Anusha Bishop <anusha.bishop@gmail.com>
2026-06-17 17:03:07 -0700

    u

--- data_processing/env/get_prism.sh
+++ data_processing/env/get_prism.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Download PRISM historical temperature data
-# Note: this downloads both annual and monthly mean data, but we ultimately use only the annual mean data
-wget -r ftp://prism.oregonstate.edu/monthly/tmean/
+# Source: https://prism.oregonstate.edu/historical/
 
+# Download historical (1895-2025) monthly tmean temperature from PRISM
+# Note: this downloads both annual and monthly mean data
+# Coding of name:
+# time_series = time series data
+# us = United States
+# an = an” refers to “all networks” and is the default PRISM data. The other option is “lt” which refers to “long term,”
+# which is an 800m monthly version of PRISM data that incorporates observations from only long-term
+# established weather stations. 
+# 800m = 800m resolution
+# tmean = mean temperature
+wget -r ftp://prism.oregonstate.edu/time_series/us/an/800m/tmean/monthly
+
+# Make temporary directory
 mkdir -p TEMP
-mv prism.oregonstate.edu/monthly/tmean/* TEMP/
+
+# Move data into temporary directory
+mv prism.oregonstate.edu/time_series/us/an/800m/tmean/monthly/* TEMP/
 
 # Unzip all .zip files within each folder, keeping the directory structure
 for year in {1895..2024}
@@ -14,8 +27,8 @@ do
 done
 
 # Move all files to the data directory
-mkdir -p ../../data/env/prism/PRISM_tmean_stable_4kmM3
-mv TEMP/* ../../data/env/prism/PRISM_tmean_stable_4kmM3
+mkdir -p ../../data/env/prism/prism_time_series_us_an_800m_tmean
+mv TEMP/* ../../data/env/prism/prism_time_series_us_an_800m_tmean
 
 # Remove the TEMP directory and the prism.oregonstate.edu directory
 rm -r TEMP prism.oregonstate.edu
